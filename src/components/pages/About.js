c6d037942620471b09270e859e75691b60f0a539
1997808 <khanhhuyle22@gmail.com>
2020-03-06 10:20:29 +0700

    react-router-dom

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. It's part of the ReactJS crash course</p>
+    </React.Fragment>
+  )
+}
+
+export default About
+
